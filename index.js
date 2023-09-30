012a5959ad92b39fc41e4666dedeb1cb2239cec5
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-09-30 12:24:46 +0200

    add change side functionality

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+const diceEl = document.querySelector('.dice')
+if (diceEl) {
+let side = 1;
+const changeSide = () => {
+    if (side > 5) {
+        side = 1;
+    } else {
+        side += 1;
+    }
+    diceEl.src = `img/side${side}.svg`;
+};
+document.addEventListener('keydown', changeSide);
+}
